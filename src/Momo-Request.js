0e77e1c13eea516a66e03763cdd2139ad7e683eb
Gabriel <gabriel@totendev.com>
2012-08-03 00:21:10 -0300

    add with uppercase

---
+++ src/Momo-Request.js
@@ -0,0 +1,60 @@
+//
+// Momo-request.js — Momo
+// today is 08/01/12, it is now 9:25 PM
+// created by TotenDev
+// see LICENSE for details.
+//
+
+var url = require('url'),
+	assert = require("assert");
+/**
+* Initialize MomoRequest function
+* @param string(url) url - Url to be requested - REQUIRED
+* @param function callback - End point to fecth cronjob list - REQUIRED
+* @param-cb bool callback.status - If request has finished with success or not - OPTIONAL
+* @param-cb string callback.response - Response (errored or not) - OPTIONAL
+* @param string method - Method to be used on http request - Default: GET - OPTIONAL
+**/
+module.exports = function (url,callback,method) { return new MomoRequest(url,callback,method); }
+function MomoRequest(url,callback,method) {
+	assert.ok(url,"No 'url' specified on MomoRequest initialization, this is a **REQUIRED** value");
+	assert.ok(callback,"No 'callback' specified on MomoRequest initialization, this is a **REQUIRED** value");
+	//
+	MomoRequest.simpleRequest(url,(method ? method : "GET"),callback);
+};
+
+/**
+* Get Request
+* @param string(url) url - Url to be requested - REQUIRED
+* @param string methodType - Method to be used on http request - REQUIRED
+* @param function callback - End point to fecth cronjob list - REQUIRED
+* @param-cb bool callback.status - If request has finished with success or not - OPTIONAL
+* @param-cb string callback.response - Response (errored or not) - OPTIONAL
+**/
+MomoRequest.simpleRequest = function simpleRequest(theURL,methodType,callback) {
+	assert.ok(theURL,"No 'url' specified on MomoRequest 'simpleRequest()' function, this is a **REQUIRED** value");
+	assert.ok(methodType,"No 'methodType' specified on MomoRequest 'simpleRequest()' function, this is a **REQUIRED** value");
+	assert.ok(callback,"No 'callback' specified on MomoRequest 'simpleRequest()' function, this is a **REQUIRED** value");
+	//Get if is http or https
+	var http = null, port = 80;
+	var requestURL = url.parse(theURL);
+	if (requestURL.protocol == "https:") { http = require('https'); port = 443; }
+	else { http = require('http'); }
+	//Make options from url
+	var responsed = false;
+	var containerData = "";
+	var options = { host: requestURL['host'], port: port, path:requestURL['path'], method: methodType };
+	var request = http.request(options,function (response) {
+		response.on('data',function (data) { containerData+=data; });
+		response.on('error',function (err) {
+			if (responsed == false) { responsed = true; callback(false,err); }
+		});
+		response.on('end',function () {
+			if (responsed == false) { responsed = true; callback(true,containerData); }
+		});
+	});
+	request.on('error',function (err) {
+		if (responsed == false) { responsed = true; callback(false,err); }
+	});
+	request.end();
+};
